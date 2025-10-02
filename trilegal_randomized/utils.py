3917e39b76a0b7ac784df980d847f87ed01e9ae4
Jim Chiang <jchiang@slac.stanford.edu>
2025-10-02 07:53:28 -0700

    apply original region selection to sampled locations

--- trilegal_randomized/utils.py
+++ trilegal_randomized/utils.py
@@ -1,22 +1,30 @@
 import numpy as np
 import astropy.units as u
+import lsst.sphgeom
 
 __all__ = ["RandomSkyPositions"]
 
 
 class RandomSkyPositions:
 
-    def __init__(self, ra0, dec0, radius, npts=None, seed=None):
-        self.AA = np.matrix([[np.cos(ra0), -np.sin(ra0), 0],
-                             [np.sin(ra0),  np.cos(ra0), 0],
-                             [0, 0, 1]])
-        self.BB = np.matrix([[-np.sin(dec0), 0, np.cos(dec0)],
-                             [0, 1, 0],
-                             [ np.cos(dec0), 0, np.sin(dec0)]])  # noqa: E201
-        self.radius = radius
+    def __init__(self, region, npts=None, seed=None):
+        self.region = region
+        self._extract_sampling_region_info()
         self.npts = npts
         self.rng = np.random.Generator(np.random.PCG64(seed))
         self.zz = np.matrix([[0], [0], [1]])
+
+    def _extract_sampling_region_info(self):
+        disk = self.region._get_bounding_disk()
+        ra0 = disk.ra*u.degree
+        dec0 = disk.dec*u.degree
+        self.AA = np.matrix([[np.cos(ra0), -np.sin(ra0), 0],
+                             [np.sin(ra0), np.cos(ra0), 0],
+                             [0, 0, 1]])
+        self.BB = np.matrix([[-np.sin(dec0), 0, np.cos(dec0)],
+                             [0, 1, 0],
+                             [np.cos(dec0), 0, np.sin(dec0)]])
+        self.radius = disk.radius
 
     def __iter__(self):
         self.count = 0
@@ -26,24 +34,24 @@ class RandomSkyPositions:
         if self.npts is not None and self.count >= self.npts:
             raise StopIteration
         self.count += 1
-        theta = np.arccos(1.0 - self.rng.uniform()*(1 - np.cos(self.radius)))
-        phi = 2.0*np.pi*self.rng.uniform()*u.radian
-        CC = np.matrix([[np.cos(phi), -np.sin(phi), 0],  # noqa: N806
-                        [np.sin(phi),  np.cos(phi), 0],
-                        [0, 0, 1]])
-        DD = np.matrix([[ np.cos(theta), 0, np.sin(theta)],  # noqa: E201, N806
-                        [0, 1, 0],
-                        [-np.sin(theta), 0, np.cos(theta)]])
-        rr = self.AA @ self.BB @ CC @ DD @ self.zz
+        while True:
+            theta = np.arccos(1 - self.rng.uniform()*(1 - np.cos(self.radius)))
+            phi = 2.0*np.pi*self.rng.uniform()*u.radian
+            CC = np.matrix([[np.cos(phi), -np.sin(phi), 0],  # noqa: N806
+                            [np.sin(phi), np.cos(phi), 0],
+                            [0, 0, 1]])
+            DD = np.matrix([[np.cos(theta), 0, np.sin(theta)],  # noqa: N806
+                            [0, 1, 0],
+                            [-np.sin(theta), 0, np.cos(theta)]])
+            rr = self.AA @ self.BB @ CC @ DD @ self.zz
+            unit_vec = lsst.sphgeom.UnitVector3d(*rr.getA()[:, 0])
+            if self.region._convex_polygon.contains(unit_vec):
+                break
         ra = np.arctan2(rr[1, 0], rr[0, 0])*180./np.pi
         dec = np.arcsin(rr[2, 0])*180./np.pi
         return float(ra), float(dec)
 
     @staticmethod
     def make_generator(region, npts=None, seed=None):
-        disk = region._get_bounding_disk()
-        ra0 = disk.ra*u.degree
-        dec0 = disk.dec*u.degree
-        radius = disk.radius
-        generator = RandomSkyPositions(ra0, dec0, radius, npts=npts, seed=seed)
+        generator = RandomSkyPositions(region, npts=npts, seed=seed)
         return generator.__iter__()
